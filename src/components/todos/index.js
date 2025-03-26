3463c230dfdda4eadc4e762ccb596f101a7ba371
Mateus Macedo <mateusmacedobatista05@gmail.com>
2025-03-26 16:45:23 -0300

    Projeto todo-list com react

---
+++ src/components/todos/index.js
@@ -0,0 +1,22 @@
+import{ todos } from "./todosDados";
+import { useState } from "react";
+
+function TodoList() {
+    const [todosList, setTodosList] = useState()
+    return (
+       <div className="TodoList">
+        {todos.map((todo) => (
+            <div className="todo">
+                <div className="conteudo">
+                    <p className="texto">{todo.text}</p>
+                    <p className="category">({todo.category})</p>
+                </div>  
+                <button>Completar</button>
+                <button>X</button>  
+            </div>   
+        ) )}
+       </div> 
+    )
+}
+
+export default TodoList
---
+++ src/components/todos/todosDados.js
@@ -0,0 +1,20 @@
+export const todos = [
+    {
+        id: 1,
+        text: "Fazer atividade de matemática",
+        category: "Faculdade",
+        isCompleted: false,
+    },
+    {
+        id: 2,
+        text: "Iniciar estudo de node.js",
+        category: "Trabalho",
+        isCompleted: false,
+    },
+    {
+        id: 3,
+        text: "Ir na academia",
+        category: "Pessoal",
+        isCompleted: false,
+    },
+]
